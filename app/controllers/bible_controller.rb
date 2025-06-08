616ca5cc161769e962350a03d61a201b3a15ec48
Miguel A Jimenez <miguelajimenez@yahoo.com>
2025-06-07 18:43:21 -0700

    Add bible controller

---
+++ app/controllers/bible_controller.rb
@@ -0,0 +1,61 @@
+class BibleController < ApplicationController
+
+  def enter_verse
+    # Renders enter_verse.html.erb by default
+  end
+
+  def select_book
+    @books = BibleReader.book_names
+  end
+
+  def set_book
+    session[:book] = params[:book]&.downcase
+    if BibleReader.book_names.include?(session[:book])
+      redirect_to bible_select_category_path
+    else
+      flash[:alert] = "Invalid book selected"
+      redirect_to bible_select_book_path
+    end
+  end
+
+  def select_category
+    # For now, we only support "find_verse"
+  end
+
+  def set_category
+    session[:category] = params[:category]
+    if session[:category] == "find_verse"
+      redirect_to bible_enter_verse_path
+    else
+      flash[:alert] = "Unsupported action"
+      redirect_to bible_select_category_path
+    end
+  end
+
+  def enter_verse
+    # Displays form to input verse reference
+  end
+
+  def show_verse
+    @reference = params[:reference]
+    @book = session[:book] || "exodo"
+
+    if @reference.blank?
+      @error = "Por favor ingrese un versículo."
+      return render :enter_verse
+    end
+
+    begin
+      @verse = BibleReader.find_one_verse(@book, @reference)
+    rescue => e
+      Rails.logger.error("Verse lookup failed: #{e.message}")
+      @error = "Versículo no encontrado o formato inválido."
+    end
+
+    render :enter_verse
+  end
+
+
+  def test_form
+  end
+end
