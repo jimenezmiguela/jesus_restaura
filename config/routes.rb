01233a5c8f7213db5815651685560f3bd18c23d0
Miguel A Jimenez <miguelajimenez@yahoo.com>
2025-06-07 18:47:12 -0700

    Add bible reader routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "bible#select_book"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get  "/bible/book",      to: "bible#select_book",     as: :bible_select_book
+  post "/bible/book",      to: "bible#set_book",        as: :bible_set_book
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get  "/bible/category",  to: "bible#select_category", as: :bible_select_category
+  post "/bible/category",  to: "bible#set_category",    as: :bible_set_category
+
+  get  "/bible/verse",     to: "bible#enter_verse",     as: :bible_enter_verse
+  post "/bible/verse",     to: "bible#show_verse",      as: :bible_show_verse
 end
